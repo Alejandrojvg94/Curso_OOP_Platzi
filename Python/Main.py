4ad90b26b48cf2b84316d47e98b1f371baec6316
Alejandro Velasco <alejandrovelasco094@gmail.com>
2022-01-31 18:41:03 -0500

    Constructor creation in Java and JS and Python

--- Python/Main.py
+++ Python/Main.py
@@ -1,18 +1,20 @@
 from Car import Car
 from Account import Account
+from Car import Car
+
 if __name__ == "__main__":
-    car = Car()
-    car.license = "AMS234"
-    car.driver = "Andres Gonzales"
+    car = Car("AMS234", Account("Andres Gonzales", "10617892"))
+
     print(vars(car))
+    print(vars(car.driver))
 
-    car2 = Car()
-    car2.license = "ZUK78D"
-    car2.driver = "Jeremias Sprinfil"
+    # car2 = Car()
+    # car2.license = "ZUK78D"
+    # car2.driver = "Jeremias Sprinfil"
 
-    print(vars(car2))
+    # print(vars(car2))
 
-    account = Account()
-    account.document = 106176788
-    account.name = "Alejandro Butifarra"
-    print(vars(account))
+    # account = Account()
+    # account.document = 106176788
+    # account.name = "Alejandro Butifarra"
+    # print(vars(account))
