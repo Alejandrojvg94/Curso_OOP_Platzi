516d45318ee8f3f83cca8ce4a30a88f3e7797bd2
Alejandro Velasco <alejandrovelasco094@gmail.com>
2022-02-03 04:19:55 -0500

    Resagados

---
+++ Js/UberPool.js
@@ -0,0 +1,9 @@
+class UberPool extends Car{
+    constructor(license, driver, brand, model){
+        super(license,driver)
+        this.model = model;
+        this.brand = brand;
+    }
+
+        
+}
